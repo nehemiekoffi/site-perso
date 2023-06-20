8b09826c67b39c2c97d58a374d6904da3a0f8309
DevDams <ad.aimedesire@gmail.com>
2023-06-20 18:49:04 +0000

    new design

---
+++ components/BottomNavigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fixed bottom-10 left-0 w-full h-fit flex items-center justify-center">
+    <div
+      class="w-96 h-12 rounded-full backdrop-blur-md bg-white/10 shadow-2xl drop-shadow-xl backdrop-blur-md border border-neutral-700 flex items-center gap-x-4 px-2">
+      <button
+        :class="currentPage === 'HOME' ? 'w-1/4 h-8 bg-black text-sm text-white font-semibold rounded-full' : 'w-1/4 h-8 bg-neutral-00 text-sm text-neutral-400 font-semibold rounded-full'"
+        @click="() => navigate('HOME')">Home</button>
+      <button
+        :class="currentPage === 'ABOUT' ? 'w-1/4 h-8 bg-black text-sm text-white font-semibold rounded-full' : 'w-1/4 h-8 bg-neutral-00 text-sm text-neutral-400 font-semibold rounded-full'"
+        @click="() => navigate('ABOUT')">About</button>
+      <button
+        :class="currentPage === 'WORK' ? 'w-1/4 h-8 bg-black text-sm text-white font-semibold rounded-full' : 'w-1/4 h-8 bg-neutral-00 text-sm text-neutral-400 font-semibold rounded-full'"
+        @click="() => navigate('WORK')">Work</button>
+      <button
+        :class="currentPage === 'CONTACT' ? 'w-1/4 h-8 bg-black text-sm text-white font-semibold rounded-full' : 'w-1/4 h-8 bg-neutral-00 text-sm text-neutral-400 font-semibold rounded-full'"
+        @click="() => navigate('CONTACT')">Contact</button>
+    </div>
+  </div>
+</template>
+<script setup>
+const currentPage = ref('HOME')
+
+const navigate = (page) => {
+  currentPage.value = page
+}
+</script>
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,76 @@
 <template>
-  <div class="bg-[#232323] w-full min-h-screen pb-16">
+  <div class="bg-[#0A0A0A] w-full min-h-screen pb-16">
     <div class="w-7/12 mx-auto pt-16">
-      <div class="w-24 mx-auto h-24 bg-gray-100 rounded-full"></div>
-      <h1 class="text-5xl text-white text-center font-bold mt-5">Adams Aimé-Désiré</h1>
-      <h1 class="text-xl text-neutral-400 text-center font-normal mt-2">Javascript developer</h1>
-      <div class="flex items-center justify-center gap-x-2 mt-4">
-        <div class="w-2 h-2 rounded-full bg-green-500"></div>
-        <p class="text-sm text-zinc-300 text-center font-normal">Available for new opportunities</p>
+      <div class="relative w-44 h-56 mx-auto h-24 rounded-md border border-stone-600 p-1">
+        
+        <img src="~/assets/images/profil.jpg" alt="artboard" class="w-full h-full object-cover rounded-md">
       </div>
-      <div class="flex items-center justify-center gap-x-5 mt-5">
+      <h1 class="text-5xl text-white text-center font-bold mt-10">Adams Aimé-Désiré</h1>
+      <h1 class="text-xl text-white text-center font-semibold mt-2">Javascript developer</h1>
+      <div class="flex items-center justify-center gap-x-2 mt-4">
+        <div class="w-4 h-4 rounded-full border border-green-500 flex items-center justify-center">
+          <div class="w-2 h-2 rounded-full bg-green-500"></div>
+        </div>
+        <p class="text-sm text-zinc-400 text-center font-normal">Available for new opportunities</p>
+      </div>
+      <div class="flex items-center justify-center gap-x-10 mt-8">
         <UTooltip class="cursor-pointer" text="Twitter">
-          <div class="text-neutral-600 text-3xl">
+          <div class="text-neutral-400 text-xl flex items-center justify-center gap-x-1">
             <UIcon name="i-mdi-twitter" />
+            <span>Twitter</span>
           </div>
         </UTooltip>
         <UTooltip class="cursor-pointer" text="Linkedin">
-          <div class="text-neutral-600 text-3xl">
+          <div class="text-neutral-400 text-xl flex items-center justify-center gap-x-1">
             <UIcon name="i-mdi-linkedin" />
+            <span>Linkedin</span>
           </div>
         </UTooltip>
         <UTooltip class="cursor-pointer" text="Github">
-          <div class="text-neutral-600 text-3xl">
+          <div class="text-neutral-400 text-xl flex items-center justify-center gap-x-1">
             <UIcon name="i-mdi-github" />
+            <span>Github</span>
           </div>
         </UTooltip>
       </div>
 
-      <div class="flex items-center justify-center gap-x-3 mt-10">
-        <button class="bg-white text-black font-medium flex items-center justify-center w-64 h-11 rounded-lg">
-          Send an email
-        </button>
-        <span class="font-medium text-sm text-lg text-neutral-600">or</span>
+      <img src="~/assets/images/art_1.png" alt="artboard" class="absolute top-40 right-44 w-10">
+
+      <div class="w-96 mx-auto mt-10 flex justify-center">
         <button
-          class="bg-neutral-800 border border-neutral-700 text-white font-medium flex items-center justify-center w-64 h-11 rounded-lg">
-          Leave a message
+          class="relative w-80 h-11 rounded-lg bg-neutral-800 text-stone-500 text-center text-sm font-semibold flex items-center justify-center gap-x-2 hover:bg-neutral-700 hover:text-stone-200">
+          <!-- <div class="absolute -top-1 -right-1 w-3 h-3 rounded-full bg-blue-500"></div> -->
+          <UIcon name="i-mdi-laptop-account" class="text-xl" /> Book a 30 minutes call
+          <img src="~/assets/images/art_4.png" alt="artboard" class="absolute top-5 right-0 w-10 -rotate-12">
         </button>
       </div>
     </div>
 
-    <div class="w-7/12 mx-auto pt-16 flex justify-center">
+    <!-- <img src="~/assets/images/art_2.png" alt="artboard" class="absolute top-10 right-40 w-10 rotate-45"> -->
+
+    <div class="relative w-[600px] mx-auto pt-16">
+      <h3 class="text-lg text-white font-bold">About me</h3>
+      <p class="text-stone-400 font-normal mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem amet
+        deserunt mollitia sed ut officia minima, architecto libero excepturi pariatur quisquam ea inventore veritatis
+        velit? Similique voluptatum commodi quod repudiandae!</p>
+      <p class="text-stone-400 font-normal mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem amet
+        deserunt mollitia sed ut officia minima, architecto libero excepturi pariatur quisquam ea inventore veritatis
+        velit? Similique voluptatum commodi quod repudiandae!</p>
+    </div>
+
+    <img src="~/assets/images/art_2.png" alt="artboard" class="absolute top-80 left-72 w-10">
+
+    <div class="w-[600px] mx-auto pt-16">
+      <h3 class="text-lg text-white font-bold">Work experience</h3>
+      <p class="text-stone-400 font-normal mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem amet
+        deserunt mollitia sed ut officia minima, architecto libero excepturi pariatur quisquam ea inventore veritatis
+        velit? Similique voluptatum commodi quod repudiandae!</p>
+      <p class="text-stone-400 font-normal mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem amet
+        deserunt mollitia sed ut officia minima, architecto libero excepturi pariatur quisquam ea inventore veritatis
+        velit? Similique voluptatum commodi quod repudiandae!</p>
+    </div>
+
+    <!-- <div class="w-7/12 mx-auto pt-16 flex justify-center">
       <div class="w-8/12 flex items-start gap-x-5">
         <div class="w-24">
           <h3 class="text-base text-zinc-500 text-center font-medium">About me</h3>
@@ -96,11 +128,14 @@
         <p class="text-center text-white w-7/12 mx-auto mt-8">If you have an idea or a project and you a help, just leave me
           a message or book a 30 minutes call</p>
       </div>
-    </div>
+    </div> -->
+
+    <BottomNavigation />
   </div>
 </template>
 
 <script setup>
+import BottomNavigation from '~/components/BottomNavigation.vue';
 import Layout from '~/layouts/Layout.vue';
 
 </script>
